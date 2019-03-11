545be37d8b2bd2159a21021cd8062595dc4d4393
Stomoarch <36717488+Stomoarch@users.noreply.github.com>
2019-03-11 22:40:40 +0900

    masu logic test
    masu logic test started. 25 masues are placed. mouse.cs was altered to get the cordinates.

--- InunekoShougi/Assets/Mouse.cs
+++ InunekoShougi/Assets/Mouse.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Mouse : MonoBehaviour
 {
-
+    public float xzahyou;
+    public float yzahyou;
 
     //マウスでドラッグするスクリプト
 
@@ -15,6 +16,10 @@ public class Mouse : MonoBehaviour
     {
             this.screenPoint = Camera.main.WorldToScreenPoint(transform.position);
             this.offset = transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+
+        xzahyou = transform.position.x;
+        yzahyou = transform.position.y;
+             
     }
 
     void OnMouseDrag()
